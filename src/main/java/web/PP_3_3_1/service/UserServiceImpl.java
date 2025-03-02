b5b3ead6f0bff105a9b19e3c88d0ecd10949e528
returnaction <obergannv@gmail.com>
2025-03-02 10:42:37 +0300

    add transactional

--- src/main/java/web/PP_3_3_1/service/UserServiceImpl.java
+++ src/main/java/web/PP_3_3_1/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package web.PP_3_3_1.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.PP_3_3_1.model.User;
@@ -18,6 +19,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void add(User user) {
         userRepository.save(user);
     }
@@ -32,11 +34,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.findById(id).get();
     }
 
+    @Transactional
     @Override
     public void update(User user) {
         userRepository.save(user);
     }
 
+    @Transactional
     @Override
     public void delete(Long id) {
         userRepository.deleteById(id);
